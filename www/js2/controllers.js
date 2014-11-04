78684f078748caaf843b7544f4f8c55d161fbcb2
RMase <rob.mase7@gmail.com>
2014-11-04 15:42:40 -0700

    Adding Class Schedule [axof:147]

---
+++ www/js2/controllers.js
@@ -0,0 +1,32 @@
+angular.module('starter.controllers', [])
+
+.controller('ToDoListCtrl', function($scope,$ionicModal) {
+	$scope.toDoListItems = [{
+
+  }];
+
+$scope.AddItem = function(data){
+	$scope.toDoListItems.push({task:data.newItem,status:'not done'});
+	 data.newItem = ' ';
+         $scope.closeModal();
+  };
+  
+$ionicModal.fromTemplateUrl('modal.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+  
+  $scope.openModal = function() {
+    $scope.modal.show();
+  };
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+  //Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+  
+});
