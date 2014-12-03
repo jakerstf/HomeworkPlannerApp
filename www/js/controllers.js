27703b5d4c6443795e8dbd76db321a72ec4bc023
Chris Neff <cmneff136@gmail.com>
2014-12-02 22:11:14 -0700

    This is a placeholder update. I need more time to get this database thing to work and build the Calendar controller to go with it. However, it should be worth noting that I found out what was breaking our code earlier today and made it work again so I still made a meaningful contribution. [axof:168]

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -79,6 +79,24 @@ angular.module('starter.controllers', [])
 
     })
 
+    .controller('JobsCtrl', function($scope, JobService) {
+        JobService.titles().then(function(data) {
+            //this will execute when the
+            //AJAX call completes.
+            $scope.titles = data;
+            console.log(data);
+        });
+    })
+
+    .controller('JobDetailCtrl', function($scope, $stateParams, JobService) {
+        JobService.getJob($stateParams.jobId).then(function(data) {
+            //this will execute when the
+            //AJAX call completes.
+            $scope.job = data;
+            console.log(data);
+        });
+    })
+
     .controller('TodoCtrl', function($scope,$ionicModal) {
 //<<<<<<< HEAD
         $scope.todos = [
---
+++ www/js/services.js
@@ -0,0 +1,27 @@
+angular.module('starter.services', [])
+
+/**
+ * A simple example service that returns some data.
+ */
+    .factory('JobService', function($http) {
+        return {
+            titles: function() {
+                //since $http.get returns a promise,
+                //and promise.then() also returns a promise
+                //that resolves to whatever value is returned in it's
+                //callback argument, we can return that.
+                return $http.get('http://dgm3790.iriscouch.com/test_assignment_db/_design/apiv1/_view/jobs')
+                    .then(function(result) {
+                        //  console.log(result.data.rows);
+                        return result.data.rows;
+                    });
+            },
+            getJob: function(jobId) {
+                return $http.get('http://dgm3790.iriscouch.com/test_assignment_db/' + jobId)
+                    .then(function(result) {
+                        //console.log(result.data.data);
+                        return result.data.data;
+                    });
+            }
+        }
+    });
